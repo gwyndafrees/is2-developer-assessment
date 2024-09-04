38b33c1d46adb7eb3b59795a387533057a0e60ec
Gwyn Rees <gwyndaf.ch.rees@gmail.com>
2024-09-04 03:42:41 +0100

    Added the Notes entity to the context and seeded it for point 4. Also added a `Notes` navigation property to the Policy model.

--- DataExporter/ExporterDbContext.cs
+++ DataExporter/ExporterDbContext.cs
@@ -1,31 +1,46 @@
 ﻿using DataExporter.Model;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 
+namespace DataExporter;
 
-namespace DataExporter
+public class ExporterDbContext : DbContext
 {
-    public class ExporterDbContext : DbContext
+    public DbSet<Policy> Policies { get; set; }
+
+    public DbSet<Note> Notes { get; set; }
+
+    public ExporterDbContext(DbContextOptions<ExporterDbContext> options) : base(options)
+    { 
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
     {
-        public DbSet<Policy> Policies { get; set; }
-
-        public ExporterDbContext(DbContextOptions<ExporterDbContext> options) : base(options)
-        { 
-        }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder options)
-        {
-            options.UseInMemoryDatabase("ExporterDb");
-        }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Policy>().HasData(new Policy() { Id = 1, PolicyNumber = "HSCX1001", Premium = 200, StartDate = new DateTime(2024, 4, 1) },
-                new Policy() { Id = 2, PolicyNumber = "HSCX1002", Premium = 153, StartDate = new DateTime(2024, 4, 5) },
-                new Policy() { Id = 3, PolicyNumber = "HSCX1003", Premium = 220, StartDate = new DateTime(2024, 3, 10) },
-                new Policy() { Id = 4, PolicyNumber = "HSCX1004", Premium = 200, StartDate = new DateTime(2024, 5, 1) },
-                new Policy() { Id = 5, PolicyNumber = "HSCX1005", Premium = 100, StartDate = new DateTime(2024, 4, 1) });
-
-            base.OnModelCreating(modelBuilder);
-        }
+        options.UseInMemoryDatabase("ExporterDb")
+            .ConfigureWarnings(warnings => warnings.Ignore(InMemoryEventId.TransactionIgnoredWarning));
+    }
+        
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Note>()
+            .HasOne<Policy>()
+            .WithMany(p => p.Notes)
+            .HasForeignKey(n => n.PolicyId);
+            
+        modelBuilder.Entity<Policy>()
+            .HasData(new Policy { Id = 1, PolicyNumber = "HSCX1001", Premium = 200, StartDate = new DateTime(2024, 4, 1) },
+                new Policy { Id = 2, PolicyNumber = "HSCX1002", Premium = 153, StartDate = new DateTime(2024, 4, 5) },
+                new Policy { Id = 3, PolicyNumber = "HSCX1003", Premium = 220, StartDate = new DateTime(2024, 3, 10) },
+                new Policy { Id = 4, PolicyNumber = "HSCX1004", Premium = 200, StartDate = new DateTime(2024, 5, 1) },
+                new Policy { Id = 5, PolicyNumber = "HSCX1005", Premium = 100, StartDate = new DateTime(2024, 4, 1) });
+            
+        modelBuilder.Entity<Note>().HasData(
+            new Note { Id = 1, Text = "Policy 1 Note 1", PolicyId = 1 },
+            new Note { Id = 2, Text = "Policy 1 Note 2", PolicyId = 1 },
+            new Note { Id = 3, Text = "Policy 2 Note 1", PolicyId = 2 },
+            new Note { Id = 4, Text = "Policy 3 Note 1", PolicyId = 3 },
+            new Note { Id = 5, Text = "Policy 5 Note 1", PolicyId = 5 });
+            
+        base.OnModelCreating(modelBuilder);
     }
 }
