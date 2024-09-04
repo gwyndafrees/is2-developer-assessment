def051ee872e314937fc878e0c5e70d58773841d
Gwyn Rees <gwyndaf.ch.rees@gmail.com>
2024-09-04 02:39:25 +0100

    Added missing GetPolicies tests

--- DataExporter.Tests/Controllers/PoliciesControllerTests.cs
+++ DataExporter.Tests/Controllers/PoliciesControllerTests.cs
@@ -71,4 +71,79 @@ public class PoliciesControllerTests
         // Assert
         result.Should().BeOfType<NotFoundResult>();
     }
+    
+    [Fact]
+    public async Task GetPolicies_ShouldReturnOkObjectResult_WhenPoliciesExist()
+    {
+        // Arrange
+        var readPolicyDto = new ReadPolicyDto
+        {
+            PolicyNumber = "123",
+            Premium = 100,
+            StartDate = DateTime.Now
+        };
+    
+        _policyService.ReadPoliciesAsync().Returns(new List<ReadPolicyDto> { readPolicyDto });
+    
+        // Act
+        var result = await _controller.GetPolicies();
+    
+        // Assert
+        result.Should().BeOfType<OkObjectResult>();
+    }
+    
+    [Fact]
+    public async Task GetPolicies_ShouldReturnExpectedPolicies_WhenPoliciesExist()
+    {
+        // Arrange
+        var readPolicyDto = new ReadPolicyDto
+        {
+            PolicyNumber = "123",
+            Premium = 100,
+            StartDate = DateTime.Now
+        };
+
+        var expectedResponse = new List<ReadPolicyDto> { readPolicyDto };
+        
+        _policyService.ReadPoliciesAsync().Returns(expectedResponse);
+    
+        // Act
+        var result = await _controller.GetPolicies();
+    
+        // Assert
+        var resultValue = (result as OkObjectResult)!.Value;
+        resultValue.Should().NotBeNull();
+        resultValue.Should().BeEquivalentTo(expectedResponse);
+    }
+    
+    [Fact]
+    public async Task GetPolicies_ShouldReturnOkObjectResult_WhenNoPoliciesExist()
+    {
+        // Arrange
+        _policyService.ReadPoliciesAsync().Returns(new List<ReadPolicyDto>());
+        
+        // Act
+        var result = await _controller.GetPolicies();
+        
+        // Assert
+        result.Should().BeOfType<OkObjectResult>();
+    }
+    
+    [Fact]
+    public async Task GetPolicies_ShouldReturnEmptyList_WhenNoPoliciesExist()
+    {
+        // Arrange
+        _policyService.ReadPoliciesAsync().Returns(new List<ReadPolicyDto>());
+        
+        // Act
+        var result = await _controller.GetPolicies();
+        
+        // Assert
+        var resultValue = (result as OkObjectResult)!.Value;
+        resultValue.Should().BeOfType<List<ReadPolicyDto>>();
+        
+        var resultList = resultValue as List<ReadPolicyDto>;
+        resultList.Should().NotBeNull();
+        resultList.Count.Should().Be(0);
+    }
 }
